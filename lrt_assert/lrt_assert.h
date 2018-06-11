a9ff8e19276f999ab57e21ff525f42c63885a19b
ChocolateCharlie <aurore.amrit@gmail.com>
2018-06-11 19:23:57 +0200

    [LRT_ASSERT] Use fprintf instead of printf and format code

--- lrt_assert/lrt_assert.h
+++ lrt_assert/lrt_assert.h
@@ -1,53 +1,52 @@
-/* **************************************************************************** */
-/*										*/
-/*							THE LIB_RT_PROJECT	*/
-/*	lrt_assert.h						LRT_ASSERT	*/
-/*										*/
-/*	By : ChocolateCharlie							*/
-/*	Updated :	2017-07-02	by ChocolateCharlie			*/
-/*										*/
-/* **************************************************************************** */
+/* ************************************************************************* */
+/*                                                                           */
+/*                                                     THE LIB_RT PROJECT    */
+/*    lrt_assert.h                                             LRT_ASSERT    */
+/*                                                                           */
+/*    By :    ChocolateCharlie                                               */
+/*    Updated :    2018-06-11    by ChocolateCharlie                         */
+/*                                                                           */
+/* ************************************************************************* */
 
-/*
-** TODO :
-**	- remove inclusion of <stdio.h> and replace printf()
-**	(write informations on stderr)
-**	- remove inclusion of <stdlib> and replace abort()
-*/
+
 
 /*
 ** Include :
-**	- stdio.h	in order to use printf()
+**	EXTERNAL HEADERS : (2)
+**	- stdio.h	in order to use fprintf()
 **	- stdlib.h	in order to use abort()
+**	INTERNAL HEADERS : none
 */
 
 #include <stdio.h>
 #include <stdlib.h>
+
 
 /*
 ** Define the lrt_assert() macro
 */
 
 #ifndef	LRT_NDEBUG
-#define	lrt_assert(e)	if (!e)							\
-			{							\
-				printf( "[LIB_RT]Assertion failed: %s\n"	\
-					"Source file: %s\n"			\
-					"Enclosing function: %s\t"		\
-					"Line: %d\n",				\
-					#e, __FILE__, __func__, __LINE__);	\
-				abort();					\
-			}
+#define	lrt_assert(e)	if (!e)												\
+						{													\
+							fprintf(stderr,									\
+									"[LIB_RT] Assertion failed : %s\n"		\
+									"Source file : %s\n"					\
+									"Enclosing function : %s\t"				\
+									"Line : %d\n",							\
+									#e, __FILE__, __func__, __LINE__);		\
+							abort();										\
+						}
 #else
-#define lrt_assert(ignore)((void) 0)
+#define	lrt_assert(ignore)((void) 0)
 #endif
+
 
 /*
 ** Define state macros :
-**	- LRT_FAILURE	0		(int)
-**	- LRT_SUCCESS	non-zero	(int)
+** - LRT_FAILURE	0			(int)
+** - LRT_SUCCESS	non_zero	(int)
 */
-
 #ifndef	LRT_ASSERT_H_INCLUDED
 #define	LRT_ASSERT_H_INCLUDED
 

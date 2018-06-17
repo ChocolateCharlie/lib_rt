0462e925c18c4474fcf880696a4817c1f7ae286e
ChocolateCharlie <aurore.amrit@gmail.com>
2018-06-17 22:41:12 +0200

    [LIB_RT] Format main header

--- lib_rt.h
+++ lib_rt.h
@@ -4,9 +4,12 @@
 /*    lib_rt.h                                                     LIB_RT    */
 /*                                                                           */
 /*    By :    ChocolateCharlie                                               */
-/*    Updated :    2018-06-13    by ChocolateCharlie                         */
+/*    Updated :    2018-06-17    by ChocolateCharlie                         */
 /*                                                                           */
 /* ************************************************************************* */
+
+#ifndef	LIB_RT_H_INCLUDED
+#define	LIB_RT_H_INCLUDED
 
 /*
 ** Include :
@@ -19,14 +22,11 @@
 **	- lrt_stdio.h
 */
 
-#ifndef	LIB_RT_H_INCLUDED
-#define	LIB_RT_H_INCLUDED
-
 #include "lrt_assert/lrt_assert.h"
 #include "lrt_ctype/lrt_ctype.h"
 #include "lrt_limits/lrt_limits.h"
 #include "lrt_stdbool/lrt_stdbool.h"
 #include "lrt_stdio/lrt_stdio.h"
 
-#endif
+#endif	// LIB_RT_INCLUDED
 
